c3fe69758cd7693f12b5232eb300f9885c0b9ae7
Eugene Suleimanov <proselytear@yahoo.com>
2025-07-14 15:41:58 +0300

    STEP-1: initial commit of the project

---
+++ src/main/java/net/proselyte/catapiintegration/CatApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package net.proselyte.catapiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatApiIntegrationApplication.class, args);
+	}
+
+}
